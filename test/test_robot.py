c1713c5a92bea22222f571adc49a344c5258bdb9
clin-nexera <candy.lin@nexera-robotics.com>
2026-02-20 08:45:15 -0800

    Add test_robot.py

---
+++ test/test_robot.py
@@ -0,0 +1,14 @@
+# class TestRobot:
+#     def test_robot_move(self, capsys, connected_robot):
+#         connected_robot.move()
+#         output = capsys.readouterr().out
+#         assert "Robot is moving\n" in output
+#         assert "Robot moved\n" in output
+
+
+#     @pytest.mark.usefixtures("connect_robot")
+#     def test_robot_move_2(self, capsys, robot):
+#         robot.move()
+#         output = capsys.readouterr().out
+#         assert "Robot is moving\n" in output
+#         assert "Robot moved\n" in output
